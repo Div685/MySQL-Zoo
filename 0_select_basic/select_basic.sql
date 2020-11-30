5497a30d82e3f5c1bae34fdd4d306f094aa55ce2
Div685 <divyeshpatel685@gmail.com>
2020-11-30 11:30:57 +0300

    Add folder 0_select_basic
    - contains questions of part 0 (questions file)
    - select_basic.sql file contains answer of that questions

---
+++ 0_select_basic/select_basic.sql
@@ -0,0 +1,8 @@
+1.
+SELECT population FROM world WHERE name = 'Germany'
+
+2.
+SELECT name, population FROM world IN ('Sweden', 'Norway', 'Denmark')
+
+3. 
+SELECT name, area From world BETWEEN 200000 AND 250000
