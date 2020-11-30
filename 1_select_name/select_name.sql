312f73426ffd106d0a241e4a2ad4add3a644e4c9
Div685 <divyeshpatel685@gmail.com>
2020-11-30 12:03:19 +0300

    Add 1_select_name folder
    - Add 8 questions in question file
    - Add 8 answer in select_name.sql file

---
+++ 1_select_name/select_name.sql
@@ -0,0 +1,23 @@
+1. 
+SELECT name FROM world WHERE name LIKE 'Y%'
+
+2.
+SELECT name FROM world WHERE name LIKE '%Y'
+
+3. 
+SELECT name FROM world WHERE name LIKE '%x%'
+
+4. 
+SELECT name FROM world WHERE name LIKE '%land'
+
+5. 
+SELECT name FROM world WHERE name LIKE 'C%' and name LIKE '%ia'
+
+6. 
+SELECT name FROM world WHERE name LIKE '%oo%'
+
+7. 
+SELECT name FROM world WHERE name LIKE '%a%a%a%'
+
+8. 
+SELECT name FROM world WHERE name LIKE '_t%' ORDER BY name
