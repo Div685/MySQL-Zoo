2f40ec67575f55a49e63773b4a90db2d60799feb
Div685 <divyeshpatel685@gmail.com>
2020-11-30 12:39:27 +0300

    Add 9-15 questions
    - Add 9 to 15 answer in select_name.sql file
    - Add 9 to 15 questions in question file

--- 1_select_name/select_name.sql
+++ 1_select_name/select_name.sql
@@ -21,3 +21,24 @@ SELECT name FROM world WHERE name LIKE '%a%a%a%'
 
 8. 
 SELECT name FROM world WHERE name LIKE '_t%' ORDER BY name
+
+9.
+SELECT name FROM world WHERE name LIKE '%o__o%'
+
+10.
+SELECT name FROM world WHERE name LIKE '____'
+
+11.
+SELECT name FROM world WHERE name = capital
+
+12.
+SELECT name FROM world WHERE capital = concat(name, 'City')
+
+13.
+select capital, name from world where capital Like concat('%',name,'%');
+
+14.
+SELECT capital, name FROM world WHERE capital LIKE concat('%', name, '%') and capital > name;
+
+15.
+SELECT name, REPLACE(capital, name, '') FROM world WHERE capital LIKE concat('%', name, '%') AND capital > name;
