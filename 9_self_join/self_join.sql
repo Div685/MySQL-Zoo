41fd2c1c5abedd0d36249bd42f35c120d38aedf6
Div685 <divyeshpatel685@gmail.com>
2020-12-02 11:03:20 +0300

    Add self_join questions
    - Add question and answer 10

--- 9_self_join/self_join.sql
+++ 9_self_join/self_join.sql
@@ -52,3 +52,26 @@ FROM route a JOIN route b ON
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart' AND b.company = 'LRT'
+
+-- 10.
+
+SELECT S.num, S.company, S.name, T.num, T.company 
+FROM 
+    (SELECT DISTINCT a.num, a.company, stopb.name 
+     FROM route a JOIN route b ON (a.num = b.num and a.company = b.company) 
+                  JOIN stops stopa ON stopa.id = a.stop 
+                  JOIN stops stopb ON stopb.id = b.stop 
+     WHERE stopa.name = 'Craiglockhart' AND stopb.name <> 'Craiglockhart'
+)S
+
+JOIN
+
+    (SELECT x.num, x.company, stopy.name 
+     FROM route x JOIN route y ON (x.num = y.num and x.company = y.company) 
+                  JOIN stops stopx ON stopx.id = x.stop 
+                  JOIN stops stopy ON stopy.id = y.stop 
+     WHERE stopx.name = 'Lochend' AND stopy.name <> 'Lochend'
+    )T
+
+ON (S.name = T.name)
+ORDER BY S.num, S.name, T.num
